0576a63351c0ba3e39c2ecdbcfef274d381a4191
hizzuu <52273972+hizzuu@users.noreply.github.com>
2021-11-13 18:09:10 +0900

    Initial commit

---
+++ microservices/bff/cmd/main.go
@@ -0,0 +1,9 @@
+package main
+
+import (
+	"github.com/hizzuu/grpc-sample-bff/internal/infrastructure"
+)
+
+func main() {
+	infrastructure.ListenAndServe()
+}
---
+++ microservices/user/build/mysql/init/001_create.sql
@@ -0,0 +1,8 @@
+CREATE TABLE `users` (
+  `id` INT(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  `email` VARCHAR(255) UNIQUE NOT NULL,
+  `encrypted_password` VARCHAR(255) NOT NULL,
+  `name` VARCHAR(255) NOT NULL,
+  `created_at` DATETIME NOT NULL,
+  `updated_at` DATETIME NOT NULL
+)
