de930dff4078f53480d75fbfabd0321dfde005de
hizzuu <52273972+hizzuu@users.noreply.github.com>
2021-11-21 16:04:27 +0900

    init: set graphql error presenter

--- microservices/bff/internal/infrastructure/server.go
+++ microservices/bff/internal/infrastructure/server.go
@@ -1,13 +1,14 @@
 package infrastructure
 
 import (
-	"log"
 	"net/http"
 	"os"
 
 	"github.com/99designs/gqlgen/graphql/handler"
 	"github.com/99designs/gqlgen/graphql/playground"
-	"github.com/hizzuu/grpc-sample-bff/internal/graph/generated"
+	"github.com/hizzuu/grpc-example-bff/internal/graph/errors"
+	"github.com/hizzuu/grpc-example-bff/internal/graph/generated"
+	"github.com/hizzuu/grpc-example-bff/utils/logger"
 )
 
 const defaultPort = "8080"
@@ -24,8 +25,11 @@ func ListenAndServe(r generated.ResolverRoot) {
 			},
 		),
 	)
+
+	srv.SetErrorPresenter(errors.ErrorPresenter())
+
 	http.Handle("/", playground.Handler("GraphQL playground", "/query"))
 	http.Handle("/query", srv)
-	log.Printf("connect to http://localhost:%s/ for GraphQL playground", port)
-	log.Fatal(http.ListenAndServe(":"+port, nil))
+	logger.Log.Infof("connect to http://localhost:%s/ for GraphQL playground", port)
+	logger.Log.Fatal(http.ListenAndServe(":"+port, nil))
 }
