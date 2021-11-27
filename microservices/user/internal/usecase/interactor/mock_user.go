0e856a784217ead9fffba6ab295c4b2e44f697f7
hizzuu <52273972+hizzuu@users.noreply.github.com>
2021-11-28 01:05:05 +0900

    gem: mock

--- microservices/user/internal/usecase/interactor/mock_user.go
+++ microservices/user/internal/usecase/interactor/mock_user.go
@@ -64,3 +64,18 @@ func (mr *MockUserInteractorMockRecorder) Get(ctx, id interface{}) *gomock.Call 
 	mr.mock.ctrl.T.Helper()
 	return mr.mock.ctrl.RecordCallWithMethodType(mr.mock, "Get", reflect.TypeOf((*MockUserInteractor)(nil).Get), ctx, id)
 }
+
+// GetByEmailAndPassword mocks base method.
+func (m *MockUserInteractor) GetByEmailAndPassword(ctx context.Context, email, password string) (*domain.User, error) {
+	m.ctrl.T.Helper()
+	ret := m.ctrl.Call(m, "GetByEmailAndPassword", ctx, email, password)
+	ret0, _ := ret[0].(*domain.User)
+	ret1, _ := ret[1].(error)
+	return ret0, ret1
+}
+
+// GetByEmailAndPassword indicates an expected call of GetByEmailAndPassword.
+func (mr *MockUserInteractorMockRecorder) GetByEmailAndPassword(ctx, email, password interface{}) *gomock.Call {
+	mr.mock.ctrl.T.Helper()
+	return mr.mock.ctrl.RecordCallWithMethodType(mr.mock, "GetByEmailAndPassword", reflect.TypeOf((*MockUserInteractor)(nil).GetByEmailAndPassword), ctx, email, password)
+}
