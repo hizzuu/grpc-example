5f37c94f499de32495a3c6909ec951ba9c536b23
hizzuu <52273972+hizzuu@users.noreply.github.com>
2021-11-28 01:07:43 +0900

    add: ctx keys and jwt keys

---
+++ microservices/bff/internal/graph/model/context.go
@@ -0,0 +1,7 @@
+package model
+
+type contextKey string
+
+const CtxJwtTokenKey contextKey = "jwtToken"
+const CtxUIDKey contextKey = "jwtToken"
+const CtxAuthErrorCtxKey contextKey = "authError"
---
+++ microservices/bff/internal/graph/model/jwt.go
@@ -0,0 +1,10 @@
+package model
+
+const (
+	TypeName           string = "JWT"
+	UIDKey             string = "uid"
+	IdTokenSub         string = "id_token"
+	RefreshTokenSub    string = "refresh_token"
+	IdTokenExpSec      int64  = 3600
+	RefreshTokenExpSec int64  = 864000
+)
