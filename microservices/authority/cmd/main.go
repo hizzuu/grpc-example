62a28a9967b4ad53dcb8ccf659d233e6230cfbf2
hizzuu <52273972+hizzuu@users.noreply.github.com>
2021-11-28 01:06:35 +0900

    add: logger

--- microservices/authority/cmd/main.go
+++ microservices/authority/cmd/main.go
@@ -1,43 +1,31 @@
 package main
 
 import (
-	"crypto/rsa"
-	"crypto/x509"
-	"encoding/json"
-	"encoding/pem"
-	"fmt"
-	"os"
+	"log"
+	"net"
 
-	"github.com/lestrrat-go/jwx/jwa"
-	"github.com/lestrrat-go/jwx/jws"
-	"github.com/lestrrat-go/jwx/jwt"
+	"github.com/hizzuu/grpc-example-authority/internal/infrastructure"
+	"github.com/hizzuu/grpc-example-authority/internal/interfaces/controllers"
+	"github.com/hizzuu/grpc-example-authority/internal/usecase/interactor"
+	"github.com/hizzuu/grpc-example-authority/utils/logger"
 )
 
-var privateKey *rsa.PrivateKey
-
 func main() {
-	token := jwt.New()
-	token.Set(jwt.IssuerKey, "authority")
-	token.Set(jwt.SubjectKey, "1") // sub = user id
-	headers := jws.NewHeaders()
-	headers.Set(jws.AlgorithmKey, jwa.RS256)
-	headers.Set(jws.TypeKey, "JWT")
-	b, err := json.Marshal(token)
+	authCtrl := controllers.NewAuthorityController(
+		interactor.NewAuthorityInteractor(),
+	)
+	srv := infrastructure.NewGrpcServer(authCtrl)
+	listen, err := net.Listen("tcp", ":50051")
 	if err != nil {
-		panic(err)
+		logger.Log.Panic(err)
 	}
-	signedToken, err := jws.Sign(b, jwa.RS256, privateKey, jws.WithHeaders(headers))
-	if err != nil {
-		panic(err)
+	if err := srv.Serve(listen); err != nil {
+		logger.Log.Panic(err)
 	}
-	fmt.Println(string(signedToken))
 }
 
 func init() {
-	block, _ := pem.Decode([]byte(os.Getenv("PRIVATE_KEY")))
-	key, err := x509.ParsePKCS1PrivateKey(block.Bytes)
-	if err != nil {
-		panic(fmt.Sprintf("failed to parse private key: %s", err))
+	if err := logger.NewLogger(); err != nil {
+		log.Panic(err)
 	}
-	privateKey = key
 }
