c65af8c2fc9bf43952e7bc50015aeba13b2994bd
hizzuu <52273972+hizzuu@users.noreply.github.com>
2021-11-28 01:08:24 +0900

    add: auth client

--- microservices/authority/internal/infrastructure/grpc.go
+++ microservices/authority/internal/infrastructure/grpc.go
@@ -1,12 +1,15 @@
 package infrastructure
 
 import (
+	"github.com/hizzuu/grpc-example-authority/gen/pb"
+	"github.com/hizzuu/grpc-example-authority/internal/interfaces/controllers"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/reflection"
 )
 
-func NewGrpcServer() *grpc.Server {
+func NewGrpcServer(authCtrl *controllers.AuthorityController) *grpc.Server {
 	srv := grpc.NewServer()
+	pb.RegisterAuthorityServiceServer(srv, authCtrl)
 	reflection.Register(srv)
 	return srv
 }
